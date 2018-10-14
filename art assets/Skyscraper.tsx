e09110ca8efe6af1d1cd8bbbba672a081fcce590
papasmurf <joegeraltrivia@gmail.com>
2018-10-13 20:01:36 -0400

    Auto stash before merge of "master" and "origin/master"

---
+++ art assets/Skyscraper.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="Skyscraper" tilewidth="120" tileheight="120" tilecount="1" columns="1">
+ <image source="Skyscraper.png" width="120" height="120"/>
+</tileset>
