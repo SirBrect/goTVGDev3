1ebd2cf0c8ca3240005d50a260d6487750ee0889
papasmurf <joegeraltrivia@gmail.com>
2018-10-15 19:58:48 -0400

    changes

---
+++ assets/4-Way Cell Tower.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="4-Way Cell Tower" tilewidth="120" tileheight="120" tilecount="9" columns="3">
+ <image source="../4-Way Cell Tower.png" width="360" height="360"/>
+</tileset>
