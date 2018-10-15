eb6ee9887d3b1906b1c9eb3dfd3543f44632acbf
papasmurf <joegeraltrivia@gmail.com>
2018-10-15 19:57:23 -0400

    tilesets

---
+++ assets/level_3/objects.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="objects" tilewidth="120" tileheight="120" tilecount="45" columns="45">
+ <image source="../objects.png" width="5400" height="120"/>
+</tileset>
