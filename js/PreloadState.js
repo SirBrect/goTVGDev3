dc11d0c65e8f2c7d785241b57bc12737ee8b5ea9
Cavell <cavellteng@gmail.com>
2018-10-16 02:46:36 -0400

    All core mechanics implemented.

--- js/PreloadState.js
+++ js/PreloadState.js
@@ -7,14 +7,14 @@ let preloadState = function(){
 preloadState.prototype.preload = function(){
 	game.load.image("play button", "assets/PlayButton.png");
 	game.load.image("win screen", "assets/WinScreen.png");
-    game.load.image("main menu screen", "assets/main_menu_art.png");
-    game.load.spritesheet("buttons", "assets/buttons.png", 16, 8);
 
 	game.load.tilemap('map', 'assets/map.json', null, Phaser.Tilemap.TILED_JSON);
 
     //  Next we load the tileset. This is just an image, loaded in via the normal way we load images:
 
     game.load.image('tiles', 'assets/maplayouts.png');
+    game.load.spritesheet('obj','assets/objects.png',120,120,45);
+    
     game.load.spritesheet('yellow_t','assets/4-way_cell_tower_yellow.png',120,120,8);
     game.load.spritesheet('blue_t','assets/4-way_cell_tower_blue.png',120,120,8);
     game.load.spritesheet('red_t','assets/4-way_cell_tower_pink.png',120,120,8);
@@ -25,13 +25,10 @@ preloadState.prototype.preload = function(){
     game.load.image('UR', 'assets/Reflect Tower UR.png');
     game.load.image('tree', 'assets/tree.png');
     game.load.image('skyscraper', 'assets/Skyscraper.png');
-    game.load.image('y_house', 'assets/yellow_house.png');
 
     //Need to be replaced in time
-    game.load.image('r_house','assets/houseright.png');
-    game.load.image('b_house','assets/colorful_building.png');
 
-    game.load.spritesheet('beam', 'assets/signalsprite.png',120,120,15);
+    game.load.spritesheet('beam', 'assets/signalsprite.png',60,60,24);
 };
 
 preloadState.prototype.create = function(){
--- js/gameplay.js
+++ js/gameplay.js
@@ -19,30 +19,33 @@ let colors = {
     YELLOW:2,
     BLUE:3
 }
-var map;
-var layer;
-var towers;
 var cur_over;
 var on_swipe;
-
+var n_beam;
+var fired;
 gameplayState.prototype.create = function(){
+    fired = false;
+    game.physics.startSystem(Phaser.Physics.ARCADE);
     game.input.onUp.add(this.mouseUp, this);
     game.input.onDown.add(this.mouseDown, this);
     map = game.add.tilemap('map');
     map.addTilesetImage('maplayouts', 'tiles', 120, 120);
-    
+
     layer = map.createLayer('ground');
-    b_towers = game.add.group();
-    r_towers = game.add.group();
-    y_towers = game.add.group();
-    rf_towers = game.add.group();
 
-    y_houses = game.add.group();
-    r_houses = game.add.group();
-    b_houses = game.add.group();
+    this.b_towers = game.add.group();
+    this.r_towers = game.add.group();
+    this.y_towers = game.add.group();
+    this.rf_towers = game.add.group();
 
-    trees = game.add.group();
-    scrapers = game.add.group();
+    this.y_houses = game.add.group();
+    this.r_houses = game.add.group();
+    this.b_houses = game.add.group();
+
+    this.trees = game.add.group();
+    this.scrapers = game.add.group();
+    this.mountains = game.add.group();
+
     for (var ol in map.objects) {
         // Loop over each object in the object layer
         for (var o in map.objects[ol]) {
@@ -50,93 +53,134 @@ gameplayState.prototype.create = function(){
             if(map.objects[ol][o].type === 'red_tower'){
                 var tow = game.add.sprite(obj.x, obj.y, 'red_t',0);
                 tow.inputEnabled = true;
-                //tow.events.onInputDown.add(this.over,tow);
-                tow.events.onInputOver.add(this.over,tow);
+                tow.events.onInputDown.add(this.over,tow);
+                //tow.events.onInputOver.add(this.over,tow);
                 tow.events.onInputOut.add(this.out,tow);
                 tow.t_type = 'tower';
                 tow.col = colors.RED;
                 tow.dir = 0;
                 tow.animations.add('spin', [0,1,2,3,4,5,6,7],13,true);
                 tow.animations.play('spin');
-                tow.beam = [];
-                r_towers.add(tow);
+                this.r_towers.add(tow);
             }
             else if(map.objects[ol][o].type === 'yellow_tower'){
                 var tow = game.add.sprite(obj.x,obj.y, 'yellow_t',0);
                 tow.inputEnabled = true;
-                //tow.events.onInputDown.add(this.over,tow);
-                tow.events.onInputOver.add(this.over,tow);
+                tow.events.onInputDown.add(this.over,tow);
+                //tow.events.onInputOver.add(this.over,tow);
                 tow.events.onInputOut.add(this.out,tow);
                 tow.t_type = 'tower';
                 tow.col = colors.YELLOW;
                 tow.dir = 0;
                 tow.animations.add('spin', [0,1,2,3,4,5,6,7],13,true);
                 tow.animations.play('spin');
-                tow.beam = [];
-                y_towers.add(tow);
+                this.y_towers.add(tow);
             }
             else if(map.objects[ol][o].type === 'blue_tower'){
                 var tow = game.add.sprite(obj.x,obj.y, 'blue_t',0);
                 tow.inputEnabled = true;
-                //tow.events.onInputDown.add(this.over,tow);
-                tow.events.onInputOver.add(this.over,tow);
+                tow.events.onInputDown.add(this.over,tow);
+                //tow.events.onInputOver.add(this.over,tow);
                 tow.events.onInputOut.add(this.out,tow);
                 tow.t_type = 'tower';
                 tow.col = colors.BLUE;
                 tow.dir = 0;
                 tow.animations.add('spin', [0,1,2,3,4,5,6,7],13,true);
                 tow.animations.play('spin');
-                tow.beam = [];
-                b_towers.add(tow);
+                this.b_towers.add(tow);
             }
             else if(map.objects[ol][o].type === 'red_house'){
-                var house = game.add.sprite(obj.x, obj.y, 'r_house');
+                var house = game.add.sprite(obj.x, obj.y, 'obj',1);
+                house.animations.add('on',[1,2],5,false);
                 house.t_type = 'house';
                 house.col = colors.RED;
-                r_houses.add(house);
+                house.on = false;
+                house.alpha = 0.25;
+                this.r_houses.add(house);
             }
             else if(map.objects[ol][o].type === 'yellow_house'){
-                var house = game.add.sprite(obj.x,obj.y, 'y_house');
+                var house = game.add.sprite(obj.x,obj.y, 'obj',5);
+                house.animations.add('on',[5,6],5,false);
                 house.t_type = 'house';
                 house.col = colors.YELLOW;
-                y_houses.add(house);
+                house.on = false;
+                house.alpha = 0.25;
+                this.y_houses.add(house);
             }
             else if(map.objects[ol][o].type === 'blue_house'){
-                var house = game.add.sprite(obj.x,obj.y, 'b_house');
+                var house = game.add.sprite(obj.x,obj.y, 'obj', 3);
+                house.animations.add('on',[3,4],5,false);
                 house.t_type = 'house';
                 house.col = colors.BLUE;
-                b_houses.add(house);
+                house.on = false;
+                house.alpha = 0.25;
+                this.b_houses.add(house);
             }
             else if(map.objects[ol][o].type === 'refection_tower'){
                 var tow = game.add.sprite(obj.x,obj.y, 'LL');
                 tow.inputEnabled = true;
-                tow.events.onInputOver.add(this.over,tow);
+                tow.events.onInputDown.add(this.over,tow);
+                //tow.events.onInputOver.add(this.over,tow);
                 tow.events.onInputOut.add(this.out,tow);
                 tow.dir = directions.UPRIGHT;
                 tow.col = colors.REF;
                 tow.t_type = 'tower';
-                tow.beam = [];
-                rf_towers.add(tow);
+                this.rf_towers.add(tow);
             }
             else if(map.objects[ol][o].type === 'sky'){
                 var scraper = game.add.sprite(obj.x,obj.y, 'skyscraper');
                 scraper.t_type = 'sky';
-                scrapers.add(tow);
+                this.scrapers.add(scraper);
             }
             else if(map.objects[ol][o].type === 'tree'){
                 var tree = game.add.sprite(obj.x,obj.y, 'tree');
                 tree.t_type = 'tree';
-                trees.add(tow);
+                game.physics.arcade.enable(tree);
+                tree.physicsBodyType = Phaser.Physics.ARCADE;
+                tree.enableBody = true;
+                tree.body.immovable = true;
+                this.trees.add(tree);
             }
             else if(map.objects[ol][o].type === 'mountain'){
                 var mountain = game.add.sprite(obj.x,obj.y);
-                mountain.inputEnabled = true;
-                mountain.events.onInputDown.add(this.over,mountain);
+                game.physics.arcade.enable(mountain);
+                mountain.physicsBodyType = Phaser.Physics.ARCADE;
+                mountain.enableBody = true;
+                mountain.body.immovable = true;
                 mountain.t_type = 'mountain';
+                this.mountains.add(mountain);
             }
             
         }
     }
+
+    game.physics.arcade.enable(this.y_houses);
+    this.y_houses.enableBody = true;
+    this.y_houses.physicsBodyType = Phaser.Physics.ARCADE;
+
+    game.physics.arcade.enable(this.b_houses);
+    this.b_houses.enableBody = true;
+    this.b_houses.physicsBodyType = Phaser.Physics.ARCADE;
+
+    game.physics.arcade.enable(this.r_houses);
+    this.r_houses.enableBody = true;
+    this.r_houses.physicsBodyType = Phaser.Physics.ARCADE;
+    
+    game.physics.arcade.enable(this.trees);
+    this.trees.enableBody = true;
+    this.trees.physicsBodyType = Phaser.Physics.ARCADE;
+
+    game.physics.arcade.enable(this.scrapers);
+    this.scrapers.enableBody = true;
+    this.scrapers.physicsBodyType = Phaser.Physics.ARCADE;
+
+    game.physics.arcade.enable(this.rf_towers);
+    this.rf_towers.enableBody = true;
+    this.rf_towers.physicsBodyType = Phaser.Physics.ARCADE;
+
+    game.physics.arcade.enable(this.mountains);
+    this.mountains.enableBody = true;
+    this.mountains.physicsBodyType = Phaser.Physics.ARCADE;
 
     layer.resizeWorld();
 };
@@ -165,7 +209,7 @@ gameplayState.prototype.swipeDone = function(cur_over) {
     
     //check the start point against the end point
     if(cur_over.t_type === 'tower'){
-        if(cur_over.col < 1){
+        if(cur_over.col === colors.REF){
             if(dirx > 0){
                 if(diry > 0){
                     console.log('Bottom right swipe detected');
@@ -224,80 +268,179 @@ gameplayState.prototype.swipeDone = function(cur_over) {
     }
 };
 gameplayState.prototype.createBeam = function(tower){
-    for(var i = 0; i < tower.beam.length; i++){
-        tower.beam[i].destroy();
-    }
-    tower.beam = [];
-
-    currentX = tower.x;
-    currentY = tower.y;
-    nextPos = this.nextPosition(tower.dir, 120);
-    hit = false;
-    while(hit === false){
-        currentX = currentX + nextPos[0];
-        currentY = currentY + nextPos[1];
-        if(currentX > 2436 || currentX < 0 || currentY < 0 || currentY > 1125){
-            hit = true;
+    if(fired === false){
+        currentX = tower.x;
+        currentY = tower.y;
+        if(tower.col === colors.RED){
+            n_beam = game.add.sprite(tower.x,tower.y,'beam',9);
+            n_beam.animations.add('beamer',[9,10,11,12,13,14],13,true);
         }
-        else{
-            var n_beam = game.add.sprite(currentX,currentY,'beam',0);
-            n_beam.animations.add('r_blue',[0,1,2,3,4],13, true);
-            if(this.checkOverlap(n_beam,trees) || this.checkOverlap(n_beam, scrapers)){
-                hit = true;
-                n_beam.destroy();
+        else if(tower.col === colors.BLUE){
+            n_beam = game.add.sprite(tower.x,tower.y,'beam',0);
+            n_beam.animations.add('beamer',[0,1,2,3,4,5],13,true);
+        }
+        else if(tower.col === colors.YELLOW){
+            n_beam = game.add.sprite(tower.x,tower.y,'beam',16);
+            n_beam.animations.add('beamer',[16,17,18,19,20,21],13,true);
+        }
+        n_beam.col = tower.col;
+        n_beam.dir = tower.dir;
+        game.physics.arcade.enable(n_beam);
+        n_beam.enableBody = true;
+        n_beam.physicsBodyType = Phaser.Physics.ARCADE;
+        n_beam.checkWorldBounds = true;
+        n_beam.outOfBoundsKill = true;
+        if(n_beam.dir === directions.UP){
+            n_beam.body.velocity.y = -360;
+        }
+        else if(n_beam.dir === directions.LEFT){
+            n_beam.body.velocity.x = -360;
+        }
+        else if(n_beam.dir === directions.RIGHT){
+            n_beam.body.velocity.x = 360;
+        }
+        else if(n_beam.dir === directions.DOWN){
+            n_beam.body.velocity.y = 360;
+        }
+        //game.physics.arcade.moveToXY(n_beam,0,tower.y,240);
+        //n_beam.updateTransform();
+        n_beam.animations.play('beamer');
+        fired = true;
+    }
+    else{
+        if(n_beam.visible === false){
+            fired = false;
+        }
+    }
+}
+
+gameplayState.prototype.collisionCallback = function(spriteA, spriteB) {
+    if(spriteB.t_type === 'tree'){
+        spriteA.kill();
+        fired = false;
+    }
+    else if(spriteB.t_type === 'sky'){
+        spriteA.kill();
+        fired = false;
+    }
+    else if(spriteB.t_type === 'house'){
+        if(spriteA.col === spriteB.col){
+            if(spriteB.on === false){
+                spriteB.on = true;
+                spriteB.alpha = 1.0;
+                spriteB.animations.play('on');
             }
-            else{
-                n_beam.animations.play('r_blue');
-                tower.beam.push(n_beam);
+        }
+    }
+    else if(spriteB.t_type === 'tower'){
+        if(spriteB.col === colors.REF){
+            if(spriteA.dir === directions.UP){
+                if(spriteB.dir === directions.DOWNRIGHT){
+                    spriteA.dir = directions.RIGHT;
+                    spriteA.body.velocity.y = 0;
+                    spriteA.body.velocity.x = 360;
+                    spriteA.x = spriteB.x + 120;
+                    spriteA.y = spriteB.y;
+                }
+                else if(spriteB.dir === directions.DOWNLEFT){
+                    spriteA.dir = directions.LEFT;
+                    spriteA.body.velocity.y = 0;
+                    spriteA.body.velocity.x = -360;
+                    spriteA.x = spriteB.x - 120;
+                    spriteA.y = spriteB.y;
+                }
+                else{
+                    spriteA.kill();
+                    fired = false;
+                }
+            }
+            else if(spriteA.dir === directions.DOWN){
+                if(spriteB.dir === directions.UPRIGHT){
+                    spriteA.dir = directions.RIGHT;
+                    spriteA.body.velocity.y = 0;
+                    spriteA.body.velocity.x = 360;
+                    spriteA.x = spriteB.x + 120;
+                    spriteA.y = spriteB.y;
+                }
+                else if(spriteB.dir === directions.UPLEFT){
+                    spriteA.dir = directions.LEFT;
+                    spriteA.body.velocity.y = 0;
+                    spriteA.body.velocity.x = -360;
+                    spriteA.x = spriteB.x - 120;
+                    spriteA.y = spriteB.y;
+                }
+                else{
+                    spriteA.kill();
+                    fired = false;
+                }
+            }
+            else if(spriteA.dir === directions.LEFT){
+                if(spriteB.dir === directions.UPRIGHT){
+                    spriteA.dir = directions.UP;
+                    spriteA.body.velocity.y = -360;
+                    spriteA.body.velocity.x = 0;
+                    spriteA.x = spriteB.x;
+                    spriteA.y = spriteB.y - 120;
+                }
+                else if(spriteB.dir === directions.DOWNRIGHT){
+                    spriteA.dir = directions.DOWN;
+                    spriteA.body.velocity.y = 360;
+                    spriteA.body.velocity.x = 0;
+                    spriteA.x = spriteB.x;
+                    spriteA.y = spriteB.y + 120;
+                }
+                else{
+                    spriteA.kill();
+                    fired = false;
+                }
+            }
+            else if(spriteA.dir === directions.RIGHT){
+                if(spriteB.dir === directions.UPLEFT){
+                    spriteA.dir = directions.UP;
+                    spriteA.body.velocity.y = -360;
+                    spriteA.body.velocity.x = 0;
+                    spriteA.x = spriteB.x;
+                    spriteA.y = spriteB.y - 120;
+                }
+                else if(spriteB.dir === directions.DOWNLEFT){
+                    spriteA.dir = directions.DOWN;
+                    spriteA.body.velocity.y = 360;
+                    spriteA.body.velocity.x = 0;
+                    spriteA.x = spriteB.x;
+                    spriteA.y = spriteB.y + 120;
+                }
+                else{
+                    spriteA.kill();
+                    fired = false;
+                }
             }
         }
     }
-}
-gameplayState.prototype.nextPosition = function(dir, distance){
-	switch(dir){
-		case directions.UP:
-			return [0, -1 * distance];
-			break;
-		case directions.RIGHT:
-			return [distance, 0];
-			break;
-		case directions.DOWN:
-			return [0, distance];
-			break;
-		case directions.LEFT:
-			return [-1 * distance, 0];
-			break;
-		default:
-			return[0,0];
-	}
-}
-
-gameplayState.prototype.OverlapsKill = function(spriteA, spriteB) {
-
-    var boundsA = spriteA.getBounds();
-    var boundsB = spriteB.getBounds();
-
-    return Phaser.Rectangle.intersects(boundsA, boundsB);
-
-}
-gameplayState.prototype.checkOverlap = function(spriteA, gSpriteB){
-    for(var i = 0, len = gSpriteB.children.length; i < len; i++){
-        if(this.OverlapsKill(spriteA,gSpriteB.children[i])){
-            return true;
-        }
+    else if(spriteB.t_type === 'mountain'){
+        spriteA.kill();
+        fired = false;
     }
-    return false;
 }
 gameplayState.prototype.over = function(tower){
     cur_over = tower;
     on_swipe = true;
-    console.log(cur_over);
+    
 }
 gameplayState.prototype.out = function(tower){
     on_swipe = false;
-    console.log(on_swipe);
 }
 gameplayState.prototype.update = function(){
+    game.physics.arcade.overlap(n_beam,this.trees,this.collisionCallback,null, this);
+    game.physics.arcade.overlap(n_beam,this.scrapers,this.collisionCallback,null, this);
+    game.physics.arcade.overlap(n_beam,this.mountains,this.collisionCallback,null, this);
+
+    game.physics.arcade.overlap(n_beam,this.rf_towers,this.collisionCallback,null, this);
+
+    game.physics.arcade.overlap(n_beam,this.r_houses,this.collisionCallback,null, this);
+    game.physics.arcade.overlap(n_beam,this.b_houses,this.collisionCallback,null, this);
+    game.physics.arcade.overlap(n_beam,this.y_houses,this.collisionCallback,null, this);
+
+
     if (this.mouseIsDown == true) {
         //get the distance between the start and end point
         var distX = Math.abs(game.input.x - this.startX);
