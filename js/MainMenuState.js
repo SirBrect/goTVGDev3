3316b4613047e8e93cffce28c7c4b8ad843774f3
Ben Krowitz <krowbar179@gmail.com>
2018-10-15 21:16:49 -0400

    Basic Commit before pull from Master

--- js/MainMenuState.js
+++ js/MainMenuState.js
@@ -2,8 +2,14 @@ let MainMenuState = function(){
 	
 };
 
-MainMenuState.prototype.create = function(){
-	button = game.add.button(game.world.centerX - 200, game.world.centerY -150, "play button", actionOnClick, this);
+MainMenuState.prototype.create = function(){\
+	let lvlOneButton = game.add.button(game.world.centerX - 256, game.world.centerY + 128, "buttons", lvlOneButtonClick, this, 0, 0, 0);
+	let lvlTwoButton = game.add.button(game.world.centerX - 256, game.world.centerY + 128, "buttons", lvlTwoButtonClick, this, 1, 1, 1);
+	let lvlThreeButton = game.add.button(game.world.centerX - 256, game.world.centerY + 128, "buttons", lvlThreeButtonClick, this, 2, 2, 2);
+	let exitButton = game.add.button(game.world.centerX - 256, game.world.centerY + 128, "buttons", exitButtonClick, this, 3, 3, 3);
+
+
+	//button = game.add.button(game.world.centerX - 200, game.world.centerY -150, "play button", actionOnClick, this);
 }
 
 actionOnClick = function(){
